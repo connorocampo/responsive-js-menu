9857e757222b9c9b88dad27173ec8ea934758e90
Connor Ocampo <connorocampo@gmail.com>
2020-05-09 13:10:08 -0400

    Add responsive menu

---
+++ menu.js
@@ -0,0 +1,18 @@
+// show and hide dropdown button
+
+function dropdownMenu() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("responsive-menu-links");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
