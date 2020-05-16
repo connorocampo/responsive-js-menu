a8c505da559c53a8e284c5f44ff562a27dd50761
Connor Ocampo <connorocampo@gmail.com>
2020-05-16 19:43:24 -0400

    Add dropdown menu event

--- menu.js
+++ menu.js
@@ -9,3 +9,17 @@ function toggleNav() {
     links.className = "nav";
   }
 }
+
+// close dropdownMenu when clicking anywhere on the screen
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("nav");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("responsive")) {
+        openDropdown.classList.remove("responsive");
+      }
+    }
+  }
+};
